6d80f61db9e9dd2c27b001f5afba44649db941bd
Manzur Alahi <manzur@dunite.se>
2022-04-25 18:20:40 +0600

    implemented generic Read and Delete

--- src/TimeTracker.Core/TimeTracking/Interfaces/ICrudService.cs
+++ src/TimeTracker.Core/TimeTracking/Interfaces/ICrudService.cs
@@ -7,4 +7,7 @@ public interface ICrudService
 {
     Task<Result<T>> CreateItem<T, TE>(T request) where T : BaseDto where TE : BaseEntity, ICrudEntity<T>, new();
     Task<Result<T>> UpdateItem<T,TE>(int id,T request) where T : BaseDto where TE : BaseEntity, ICrudEntity<T>;
+    Task<Result<T>> FindById<T,TE>(int id) where T : BaseDto where TE : BaseEntity, ICrudEntity<T>;
+    Task<Result> RemoveItem<T,TE>(int id) where T : BaseDto where TE : BaseEntity, ICrudEntity<T>;
+    IQueryable<TE> Query<TE>() where TE : BaseEntity;
 }
--- src/TimeTracker.Core/TimeTracking/Services/CrudService.cs
+++ src/TimeTracker.Core/TimeTracking/Services/CrudService.cs
@@ -1,4 +1,5 @@
-﻿using CSharpFunctionalExtensions;
+﻿using AutoMapper;
+using CSharpFunctionalExtensions;
 using Microsoft.EntityFrameworkCore;
 using TimeTracker.Core.Shared.Interfaces;
 using TimeTracker.Core.TimeTracking.Interfaces;
@@ -9,39 +10,83 @@ namespace TimeTracker.Core.TimeTracking.Services;
 public class CrudService : ICrudService
 {
     private readonly TimeTrackingContext _context;
+    private readonly IMapper _mapper;
 
-    public CrudService(TimeTrackingContext context)
+    public CrudService(TimeTrackingContext context,IMapper mapper)
     {
         _context = context;
+        _mapper = mapper;
     }
+    
+    /**
+     * Write operations
+     */
 
     public async Task<Result<T>> CreateItem<T, TE>(T request)
         where T : BaseDto where TE : BaseEntity, ICrudEntity<T>, new()
     {
         var item = new TE();
-       var initialize = item.Initialize(request);
-       if (initialize.IsFailure)
-       {
-           return Result.Failure<T>(initialize.Error);
-       }
+        var initialize = item.Initialize(request);
+        if (initialize.IsFailure)
+        {
+            return Result.Failure<T>(initialize.Error);
+        }
+
         _context.Set<TE>().Add(item);
         await _context.SaveChangesAsync();
         return Result.Success(request);
     }
 
-    public async Task<Result<T>> UpdateItem<T, TE>(int id,T request) where T : BaseDto where TE : BaseEntity, ICrudEntity<T>
+    public async Task<Result<T>> UpdateItem<T, TE>(int id, T request)
+        where T : BaseDto where TE : BaseEntity, ICrudEntity<T>
     {
         Maybe<TE?> item = await _context.Set<TE>().FirstOrDefaultAsync(x => x.Id == id);
         if (item.HasNoValue)
         {
             return Result.Failure<T>("Item not found!");
         }
+
         var update = item.Value!.Update(request);
         if (update.IsFailure)
         {
             return Result.Failure<T>(update.Error);
         }
+
         await _context.SaveChangesAsync();
         return Result.Success(request);
+    }
+
+    public async Task<Result> RemoveItem<T, TE>(int id) where T : BaseDto where TE : BaseEntity, ICrudEntity<T>
+    {
+        Maybe<TE?> item = await _context.Set<TE>().FirstOrDefaultAsync(x => x.Id == id);
+        if (item.HasNoValue)
+        {
+            return Result.Failure<T>("Item not found!");
+        }
+
+        _context.Remove(item.Value!);
+        await _context.SaveChangesAsync();
+        return Result.Success();
+    }
+
+    /**
+     * Read operations, no tracking
+     */
+
+    public async Task<Result<T>> FindById<T, TE>(int id) where T : BaseDto where TE : BaseEntity, ICrudEntity<T>
+    {
+        Maybe<TE?> item = await _context.Set<TE>().AsNoTracking().FirstOrDefaultAsync(x => x.Id == id);
+        if (item.HasNoValue)
+        {
+            return Result.Failure<T>("Item not found!");
+        }
+
+        var data = _mapper.Map<TE, T>(item.Value!);
+        return Result.Success(data);
+    }
+    
+    public IQueryable<TE> Query<TE>() where TE : BaseEntity
+    {
+        return _context.Set<TE>().AsNoTracking().AsQueryable();
     }
 }
